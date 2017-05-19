7d1a1def8392aa2a279cde61b995bf51964545a3
alaroha <hannes.alarotu@hiq.fi>
2017-05-19 10:26:49 +0300

    new features added

--- heroku-test/src/main/java/fi/ha/controller/HerokuController.java
+++ heroku-test/src/main/java/fi/ha/controller/HerokuController.java
@@ -1,15 +1,32 @@
 package fi.ha.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HerokuController {
 	
 	@RequestMapping("/")
-    public String home(Model model) {
-        model.addAttribute("teksti", "My Spring Boot Test App");
+    public String home(Model model, HttpServletResponse response, @CookieValue("myCookie") String cookie) {
+        int counter = 0;
+        if (cookie != null) {
+        	try {
+        		counter = Integer.parseInt(cookie);
+        		counter++;
+        	} catch (Exception e) {
+        		counter = 0;
+        	}
+        }
+
+		response.addCookie(new Cookie("myCookie", ""+counter));
+		model.addAttribute("counter", ""+counter);
+		model.addAttribute("header", "My Spring Boot Test App");
+		
 		return "index";
     }
 
