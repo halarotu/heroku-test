fa480fbb314c220f73af5af650d05b5f67f433ff
alaroha <hannes.alarotu@hiq.fi>
2017-05-19 11:38:49 +0300

    fixed cookie issue

--- heroku-test/src/main/java/fi/ha/controller/HerokuController.java
+++ heroku-test/src/main/java/fi/ha/controller/HerokuController.java
@@ -1,20 +1,33 @@
 package fi.ha.controller;
 
+import java.util.Arrays;
+
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HerokuController {
 	
+	private final String MYCOOKIE = "myCookie"; 
+	
 	@RequestMapping("/")
-    public String home(Model model, HttpServletResponse response, @CookieValue("myCookie") String cookie) {
-        int counter = 0;
-        if (cookie != null) {
+    public String home(Model model, HttpServletRequest request, HttpServletResponse response) {
+        
+		String cookie = "";
+		for (Cookie c : Arrays.asList(request.getCookies())) {
+			if (c.getName().equals(MYCOOKIE)) {
+				cookie = c.getValue();
+				break;
+			}
+		}
+
+		int counter = 0;
+        if (cookie != null && !cookie.isEmpty()) {
         	try {
         		counter = Integer.parseInt(cookie);
         		counter++;
@@ -23,7 +36,7 @@ public class HerokuController {
         	}
         }
 
-		response.addCookie(new Cookie("myCookie", ""+counter));
+		response.addCookie(new Cookie(MYCOOKIE, ""+counter));
 		model.addAttribute("counter", ""+counter);
 		model.addAttribute("header", "My Spring Boot Test App");
 		
