f4c9e6ade6cc283d1babea475222550e78a51d24
alaroha <hannes.alarotu@hiq.fi>
2017-05-19 13:00:03 +0300

    "folder fix"

---
+++ src/test/java/fi/ha/AppTest.java
@@ -0,0 +1,35 @@
+package fi.ha;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTest {
+	
+	@Autowired
+    private WebApplicationContext webAppContext;
+	
+	private MockMvc mockMvc;
+	 	
+	@Before
+	 public void setUp() {
+		 this.mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+	 }
+	 
+	@Test
+    public void statusOk() throws Exception {
+		mockMvc.perform(get("/"))
+            .andExpect(status().isOk());
+    }
+}
